5f7a5614f3fa22d9ebaa358c2abb5d60484bdcb8
Matthew Rich <rich.matthewJ@gmail.com>
2019-06-21 16:13:00 -0500

    basic socket working

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+
 
 const port = process.env.PORT || 9500
 
@@ -14,23 +18,52 @@ server.listen(port, () => {
     console.log("Server up on port " + port)
 });
 
-io.on('connection', (socket) => {
-    console.log("User connected")
 
-    var client = {
-        clientId: socket.id
-    }
+let currentConnections = {};
+io.on('connection', async (socket) => {
     
-    // send the client their ID to be used for direct communcation
-    io.sockets.to(client.clientId).emit("client-id", client.clientId)
-
-
-
-    io.on("friend-request", (data) => {
-        let clientId = data.clientId;
-        let sendingUser = data.sendingUser;
-
-        // send friend request alert to receiving client id
-        io.sockets.to(clientId).emit("new-friend-request", sendingUser);
+    socket.on("user-connected", async (token) => {
+        try {
+            let decodedData = await jwt.verify(token, process.env.TOKEN_SECRET);
+            currentConnections[socket.id] = decodedData.user_id;
+        } catch (e) {
+            return e
+        }
     })
+
+    socket.on("friend-request", (data) => {
+        let sender = data.sender;
+        let receiver = data.receiver;
+        let friendRequestId = data.friendRequestId;
+        let receiverIsOnline = Object.values(currentConnections).indexOf(receiver.id) >= 0;
+
+        if (receiverIsOnline) {
+            const clientId = Object.keys(currentConnections).find(key => currentConnections[key] === receiver.id);
+
+            let frPayLoad = {
+                senderData: {
+                    username: sender.username,
+                    id: sender.id
+                },
+                receiverData: {
+                    username: receiver.username,
+                    id: receiver.id
+
+                },
+                friendRequestId: friendRequestId
+            }
+            io.sockets.to(clientId).emit('new-friend-request', frPayLoad);
+
+        }
+    })
+
+    socket.on("test", (data) => {
+        console.log(data)
+    })
+
+
+    // remove the client from current connections when they disconnect
+    socket.on('disconnect', (() => {
+        delete currentConnections[socket.id];
+    }))
 })
